11b2a3c5188f607b03ac2993bba9645e1eb6993e
jimmyduang <jimmyduang@users.noreply.github.com>
2018-07-06 12:22:20 +0800

    server add
    add server

---
+++ server/game/internal/chanrpc.go
@@ -0,0 +1,25 @@
+package internal
+
+import (
+	"github.com/name5566/leaf/gate"
+)
+
+var agents = make(map[gate.Agent]struct{})
+
+func init() {
+	skeleton.RegisterChanRPC("NewAgent", rpcNewAgent)
+	skeleton.RegisterChanRPC("CloseAgent", rpcCloseAgent)
+	for a := range agents {
+		a.WriteMsg("helloworld")
+	}
+}
+
+func rpcNewAgent(args []interface{}) {
+	a := args[0].(gate.Agent)
+	_ = a
+}
+
+func rpcCloseAgent(args []interface{}) {
+	a := args[0].(gate.Agent)
+	_ = a
+}
---
+++ server/msg/msg.go
@@ -0,0 +1,17 @@
+package msg
+
+import "github.com/name5566/leaf/network/protobuf"
+
+// 使用默认的 JSON 消息处理器（默认还提供了 protobuf 消息处理器）
+var Processor = protobuf.NewProcessor()
+
+func init() {
+	// 这里我们注册了一个 JSON 消息 Hello
+	Processor.Register(&Hello{})
+}
+
+// // 一个结构体定义了一个 JSON 消息的格式
+// // 消息名为 Hello
+// type Hello struct {
+// 	Name string
+// }
